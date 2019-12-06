5dd15db597babe3814dd0a3a3b43eceb8fd326e0
nozo <nozomi.pine@gmail.com>
2019-12-06 11:42:44 +0000

    Implement initial os (6th step)

--- src/os/main.c
+++ src/os/main.c
@@ -4,7 +4,28 @@
 
 int main(void)
 {
+  static char buf[32];
+  
   puts("Hello World!\n");
+
+  while(1) {
+    puts("> ");
+    gets(buf);
+
+    /* echo command */
+    if (!strncmp(buf, "echo ", 5)) {
+      puts(buf + 5);
+      puts("\n");
+    /* exit command */
+    } else if (!strcmp(buf, "exit")) {
+      break;
+    /* unkown command */
+    } else {
+      puts("unknown command : ");
+      puts(buf);
+      puts("\n");
+    }
+  }
 
   return 0;
 }
